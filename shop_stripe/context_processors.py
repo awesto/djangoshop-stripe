c204ef8dcbef96baa70b9c4e3f748317be7df041
Jacob Rief <jacob.rief@gmail.com>
2017-06-22 15:51:58 +0200

    add SHOP_STRIPE_PREFILL for debugging

--- shop_stripe/context_processors.py
+++ shop_stripe/context_processors.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.conf import settings
 
 
 def public_keys(request):
     return {
         'SHOP_STRIPE_PUBKEY': settings.SHOP_STRIPE['PUBKEY'],
+        'SHOP_STRIPE_PREFILL': getattr(settings, 'SHOP_STRIPE_PREFILL', False)
     }
