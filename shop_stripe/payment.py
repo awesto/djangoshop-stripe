f4df6a5d3e628a25c450e4b9f5e8694b7e48e3b2
Jacob Rief <jacob.rief@gmail.com>
2017-10-09 18:31:04 +0200

    support for üayment refunding

--- shop_stripe/payment.py
+++ shop_stripe/payment.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 
-from django_fsm import transition
+from django_fsm import transition, RETURN_VALUE
 from rest_framework.exceptions import ValidationError
 import stripe
 
@@ -15,6 +15,8 @@ from shop import __version__ as SHOP_VERSION
 from shop.models.order import BaseOrder, OrderModel, OrderPayment
 from shop.money import MoneyMaker
 from shop.payment.base import PaymentProvider
+
+stripe.api_key = settings.SHOP_STRIPE['APIKEY']
 
 
 class StripePayment(PaymentProvider):
@@ -30,7 +32,7 @@ class StripePayment(PaymentProvider):
         try:
             self.charge(cart, request)
             thank_you_url = OrderModel.objects.get_latest_url()
-            js_expression = '$window.location.href="{}";'.format(thank_you_url)
+            js_expression = 'window.location.href="{}";'.format(thank_you_url)
             return js_expression
         except (KeyError, stripe.error.StripeError) as err:
             raise ValidationError(err)
@@ -41,7 +43,6 @@ class StripePayment(PaymentProvider):
         This view is invoked by the Javascript function `scope.charge()` delivered
         by `get_payment_request`.
         """
-        stripe.api_key = settings.SHOP_STRIPE['APIKEY']
         token_id = cart.extra['payment_extra_data']['token_id']
         if LooseVersion(SHOP_VERSION) < LooseVersion('0.11'):
             charge = stripe.Charge.create(
@@ -80,7 +81,8 @@ class OrderWorkflowMixin(object):
 
     def __init__(self, *args, **kwargs):
         if not isinstance(self, BaseOrder):
-            raise ImproperlyConfigured('OrderWorkflowMixin is not of type BaseOrder')
+            raise ImproperlyConfigured("class 'OrderWorkflowMixin' is not of type 'BaseOrder'")
+
         super(OrderWorkflowMixin, self).__init__(*args, **kwargs)
 
     @transition(field='status', source=['created'], target='paid_with_stripe')
@@ -88,12 +90,34 @@ class OrderWorkflowMixin(object):
         assert self.currency == charge['currency'].upper(), "Currency mismatch"
         Money = MoneyMaker(self.currency)
         amount = Money(charge['amount']) / Money.subunits
-        OrderPayment.objects.create(order=self, amount=amount, transaction_id=charge['id'], payment_method=StripePayment.namespace)
+        OrderPayment.objects.create(order=self, amount=amount, transaction_id=charge['id'],
+                                    payment_method=StripePayment.namespace)
 
     def is_fully_paid(self):
         return super(OrderWorkflowMixin, self).is_fully_paid()
 
     @transition(field='status', source='paid_with_stripe', conditions=[is_fully_paid],
-        custom=dict(admin=True, button_name=_("Acknowledge Payment")))
+                custom=dict(admin=True, button_name=_("Acknowledge Payment")))
     def acknowledge_stripe_payment(self):
         self.acknowledge_payment()
+
+    def refund_payment(self):
+        """
+        Refund the payment using Stripe's refunding API.
+        """
+        Money = MoneyMaker(self.currency)
+        filter_kwargs = {
+            'transaction_id__startswith': 'ch_',
+            'payment_method': StripePayment.namespace,
+        }
+        for payment in self.orderpayment_set.filter(**filter_kwargs):
+            refund = stripe.Refund.create(charge=payment.transaction_id)
+            if refund['status'] == 'succeeded':
+                amount = Money(refund['amount']) / Money.subunits
+                OrderPayment.objects.create(order=self, amount=-amount, transaction_id=refund['id'],
+                                            payment_method=StripePayment.namespace)
+
+        del self.amount_paid  # to invalidate the cache
+        if self.amount_paid:
+            # proceed with other payment service providers
+            super(OrderWorkflowMixin, self).refund_payment()
