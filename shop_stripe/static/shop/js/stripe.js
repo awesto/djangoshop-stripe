bdfc036746a54c917ddb4c407c3c6f9487b251cf
Jacob Rief <jacob.rief@gmail.com>
2018-11-30 15:41:27 +0100

    compatible with django-angular-2.2

--- shop_stripe/static/shop/js/stripe.js
+++ shop_stripe/static/shop/js/stripe.js
@@ -5,7 +5,7 @@ var module = angular.module('django.shop.stripe', ['angular-stripe']);
 
 // Directive <form stripe-card-form ...>
 // Must be added to the form containing the CC input fields
-module.directive('stripeCardForm', ['$http', '$log', '$q', 'stripe', function($http, $log, $q, stripe) {
+module.directive('stripeCardForm', ['$http', '$log', '$q', '$timeout', 'stripe', function($http, $log, $q, $timeout, stripe) {
 	// iterate over sibling scopes to find the scope object holding the customer's data
 	function findCustomerScope(scope) {
 		while (scope) {
@@ -31,75 +31,81 @@ module.directive('stripeCardForm', ['$http', '$log', '$q', 'stripe', function($h
 				};
 			}
 
-			$scope.prepare = function() {
-				return function(response) {
-					var deferred;
-
-					if ($scope.payment_method.payment_modifier !== 'stripe-payment'
-					  || angular.isObject($scope.payment_method.payment_data))
-						return $q.resolve(response);
-
-					// pass data from Stripe Card Form to PSP and fetch the token
-					deferred = $q.defer();
-					stripe.card.createToken($scope.payment.card).then(function(token) {
-						$log.log("Token created for card ending in ", token.card.last4);
-						$scope.payment_method.payment_data = {token_id: token.id};
-						$scope.payment.card = {name: $scope.payment.card.name};  // forget credit card number immediately
-						$scope.stripe_card_form.$setSubmitted();
-
-						// emulate a response object for the next promise handler, since `stripe.card.createToken()`
-						// returns a proprietary and incompatible token object
-						response = {
-							status: 200,
-							statusText: "OK",
-							data: {}
-						};
-						deferred.resolve(response);
-					}).catch(function(error) {
-						var fieldName = error.param;
-						$log.log(error.type + ": " + error.message);
-						if (error.type === 'card_error') {
-							$scope.stripe_card_form[fieldName].$message = error.message;
-							$scope.stripe_card_form[fieldName].$setValidity('rejected', false);
-							$scope.stripe_card_form[fieldName].$setPristine();
-						}
-						$scope.stripe_card_form.$setSubmitted();
-
-						// emulate a response object for the next promise handler, since `stripe.card.createToken()`
-						// returns a proprietary and incompatible error object
-						response = {
-							status: 422,
-							statusText: error.code,
-							data: {stripe_card_form: {}}
-						};
-						response.data.stripe_card_form[fieldName] = {};
-						deferred.reject(response);
-					});
-					return deferred.promise;
-				}
-			};
-
 			$scope.payment_method.payment_data = null;
 		}],
-		link: function(scope, element, attrs, formController) {
-			var watcher;
-			if (!angular.isObject(scope.payment_method))
-				throw new Error("Local scope can not manage the Payment Method Form. Did you forget to wrap it into a 'Set of Forms' plugin?");
+		link: {
+			pre: function(scope) {
+				scope.prepare = function() {
+					return function(response) {
+						var deferred, response;
 
-			watcher = scope.$watch('payment_method.payment_modifier', function(value) {
-				if (value !== 'stripe-payment') {
-					formController._previous_$valid = formController.$valid;
-					// enforce the Stripe form to be valid, so that the wrapping formsSet is valid
-					formController.$valid = true;
-					formController.$invalid = false;
-				} else if (formController.hasOwnProperty('_previous_$valid')) {
-					// restore the previous valid state of the form
-					formController.$valid = formController._previous_$valid;
-					formController.$invalid = !formController.$valid;
-					delete formController._previous_$valid;
-				}
-			});
-			scope.$on('$destroy', watcher);
+						if (scope.payment_method.payment_modifier !== 'stripe-payment'
+						  || angular.isObject(scope.payment_method.payment_data))
+							return $q.resolve();
+
+						// pass data from Stripe Card Form to PSP and fetch the token
+						deferred = $q.defer();
+						stripe.card.createToken(scope.payment.card).then(function(token) {
+							$log.log("Token created for card ending in ", token.card.last4);
+							scope.payment_method.payment_data = {token_id: token.id};
+							$timeout(function() {
+								// forget credit card number after 3 seconds
+								scope.payment.card = {name: scope.payment.card.name};
+								scope.stripe_card_form.$setSubmitted();
+							}, 3000);
+
+							// emulate a response object for the next promise handler, since `stripe.card.createToken()`
+							// returns a proprietary and incompatible token object
+							response = {
+								status: 200,
+								statusText: "OK",
+								data: {}
+							};
+							deferred.resolve(response);
+						}).catch(function(error) {
+							var fieldName = error.param;
+							$log.log(error.type + ": " + error.message);
+							if (error.type === 'card_error') {
+								scope.stripe_card_form[fieldName].$message = error.message;
+								scope.stripe_card_form[fieldName].$setValidity('rejected', false);
+								scope.stripe_card_form[fieldName].$setPristine();
+							}
+							scope.stripe_card_form.$setSubmitted();
+
+							// emulate a response object for the next promise handler, since `stripe.card.createToken()`
+							// returns a proprietary and incompatible error object
+							response = {
+								status: 422,
+								statusText: error.code,
+								data: {stripe_card_form: {}}
+							};
+							response.data.stripe_card_form[fieldName] = {};
+							deferred.reject(response);
+						});
+						return deferred.promise;
+					};
+				};
+			},
+			post: function(scope, element, attrs, formController) {
+				var watcher;
+				if (!angular.isObject(scope.payment_method))
+					throw new Error("Local scope can not manage the Payment Method Form. Did you forget to wrap it into a 'Set of Forms' plugin?");
+
+				watcher = scope.$watch('payment_method.payment_modifier', function (value) {
+					if (value !== 'stripe-payment') {
+						formController._previous_$valid = formController.$valid;
+						// enforce the Stripe form to be valid, so that the wrapping formsSet is valid
+						formController.$valid = true;
+						formController.$invalid = false;
+					} else if (formController.hasOwnProperty('_previous_$valid')) {
+						// restore the previous valid state of the form
+						formController.$valid = formController._previous_$valid;
+						formController.$invalid = !formController.$valid;
+						delete formController._previous_$valid;
+					}
+				});
+				scope.$on('$destroy', watcher);
+			}
 		}
 	};
 }]);
