4efde60b20a74f91dd67b5206db7f4dc9c4a251c
Jacob Rief <jacob.rief@gmail.com>
2019-02-25 21:16:02 +0100

    PEP-8 fixes

--- shop_stripe/modifiers.py
+++ shop_stripe/modifiers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from datetime import date
 from django.utils.translation import ugettext_lazy as _
 from shop.payment.modifiers import PaymentModifier
