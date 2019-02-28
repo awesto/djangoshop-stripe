c981cddf7cc71e9eee3f3000a741581152da191a
Jacob Rief <jacob.rief@gmail.com>
2019-02-28 16:27:48 +0100

    remove requirement

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     classifiers=CLASSIFIERS,
     install_requires=[
         'stripe>=1.53.0,<1.54',
-        'django-shop>=1.0,<1.1',
     ],
     packages=find_packages(),
     include_package_data=True,
